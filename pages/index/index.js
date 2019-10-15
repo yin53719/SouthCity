a40e3fd2658252b1283934c22baf27ab84fdd825
yin53719 <yin53719@qq.com>
2019-10-15 11:49:49 +0800

    页面梳理

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,11 @@
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    motto: 'Hello World'
   },
   goIntap: function(){
     wx.switchTab({
       url: '../participate/participate'　　// 页面 A
     })
-  },
-  //事件处理函数
-  bindViewTap: function() {
-
-  },
-  onLoad: function() {
-     console.log(getApp())
-  },
-  onShow: function() {
-    
-    // 调用接口
-    // const location = chooseLocation.getLocation();
   }
 })
